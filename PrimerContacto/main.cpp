bc6210b2caa104f0fdd54d45887c2482bb2d2d39
Turupawn <ahmed.hn.43@gmail.com>
2016-01-26 09:28:59 -0600

    primer commit

---
+++ PrimerContacto/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int *y = new int(20);
+
+    cout<<x<<endl;
+    cout<<&x<<endl;
+
+    cout<<y<<endl;
+    cout<<&y<<endl;
+    cout<<*y<<endl;
+
+    int arr[3];
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+
+    int *arr2 = new int[3];
+    arr2[0]=10;
+    arr2[1]=20;
+    arr2[2]=30;
+    cout<<arr2[0]<<endl;
+    cout<<arr2[1]<<endl;
+    cout<<arr2[2]<<endl;
+
+
+    return 0;
+}
