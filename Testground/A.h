8c9a4d41ac50e38e12c498336cccaa8624b4d767
Turupawn <ahmed.hn.43@gmail.com>
2016-02-17 10:04:29 -0600

    examen resuelto

---
+++ Testground/A.h
@@ -0,0 +1,13 @@
+#ifndef A_H
+#define A_H
+
+class A
+{
+    public:
+        A();
+        virtual ~A();
+    protected:
+    private:
+};
+
+#endif // A_H
---
+++ Testground/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "A.h"
+#include "B.h"
+
+using namespace std;
+
+class MiOtraClase
+{
+public:
+    int *y;
+    int z;
+    MiOtraClase()
+    {
+        y=new int(20);
+        z=30;
+    }
+    ~MiOtraClase()
+    {
+        delete y;
+    }
+};
+
+class MiClase
+{
+public:
+    int *x;
+    MiOtraClase *moc;
+    MiClase()
+    {
+        x=new int(10);
+        moc=new MiOtraClase();
+    }
+    ~MiClase()
+    {
+        delete x;
+        delete moc;
+    }
+};
+
+int main()
+{
+    cout<<pi<<endl;
+    return 0;
+}
